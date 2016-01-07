a4b1a0380201654c8c4aaf3385c08d87a43edbcc
mmaguire400 <mmaguire400@gmail.com>
2016-01-07 12:43:36 -0500

    Initial commit of project files

---
+++ client/templates/lists-menus.js
@@ -0,0 +1,8 @@
+Template.listsMenus.helpers({
+    loopLog: function () {
+        console.log('loop listsMenus.html');
+    },
+    entryLog: function () {
+        console.log('entry listsMenus.html');
+    }
+});
