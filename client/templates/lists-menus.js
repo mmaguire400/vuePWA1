07770c16a1e50414788aeba4f25a524f8c928f56
mmaguire400 <mmaguire400@gmail.com>
2016-01-07 13:00:15 -0500

    WS-144.2608 <mark@WIN-05EC7R0O70G Overwrite remote C:\Dev\Projects\meteor\RosiesKitchen to local

--- client/templates/lists-menus.js
+++
@@ -1,8 +0,0 @@
-Template.listsMenus.helpers({
-    loopLog: function () {
-        console.log('loop listsMenus.html');
-    },
-    entryLog: function () {
-        console.log('entry listsMenus.html');
-    }
-});
